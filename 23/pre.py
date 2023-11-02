0ef7f979e92697ec96cc4fcd6cec00fab20681b4
Arif Raihan Mahee <arifrayhan911@gmail.com>
2023-11-02 22:57:03 +0600

    Add files via upload

---
+++ 23/pre.py
@@ -0,0 +1,5 @@
+import os
+os.system("pip install selenium==4.15.1")
+os.system("pip install undetected-chromedriver==3.5.3")
+os.system("pip install pandas==2.1.2")
+os.system("pip install chromedriver_autoinstaller==0.6.2")
